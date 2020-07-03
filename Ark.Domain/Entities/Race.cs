4f7f1b2e32b6855cf4e0c2ab98a8e4eaeacdefa5
daniel.joris <danieljoris08@gmail.com>
2020-07-03 00:57:13 -0300

    Added Race and Specie entities.

--- Ark.Domain/Entities/Race.cs
+++ Ark.Domain/Entities/Race.cs
@@ -1,6 +1,16 @@
+using Ark.Shared;
+
 namespace Ark.Domain.Entities
 {
-    public class Race
+    public class Race : Entity
     {
+        public string Description { get; private set; }
+        public Specie Specie { get; private set; }
+
+        public Race(string description, Specie specie)
+        {
+            this.Description = description;
+            this.Specie = specie;
+        }
     }
 }
--- Ark.Domain/Entities/Specie.cs
+++ Ark.Domain/Entities/Specie.cs
@@ -2,5 +2,11 @@ namespace Ark.Domain.Entities
 {
     public class Specie
     {
+        public Specie(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; private set; }
     }
 }
