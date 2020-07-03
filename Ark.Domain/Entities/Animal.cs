cc4b774adc9bae37b34bc48d4d483a24bfeb9626
daniel.joris <danieljoris08@gmail.com>
2020-07-03 01:02:39 -0300

    Added Birthday methods, AnimalDied method and Created/Updated properties.

--- Ark.Domain/Entities/Animal.cs
+++ Ark.Domain/Entities/Animal.cs
@@ -1,27 +1,56 @@
 using System;
+using Ark.Shared;
 
 namespace Ark.Domain.Entities
 {
-    public class Animal
+    public class Animal : Entity
     {
+        public Animal(string name, string approximateAge, bool castrated)
+        {
+            Name = name;
+            ApproximateAge = approximateAge;
+            Castrated = castrated;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+
         public string Name { get; private set; }
         public Specie Specie { get; private set; }
         public Race Race { get; private set; }
-        public DateTime Birthday { get; private set; }
+        public DateTime? Birthday { get; private set; }
+        public bool IsBirthday => Birthday.HasValue ? BirthdayIsToday() : false;
         public string ApproximateAge { get; private set; }
         public bool Castrated { get; private set; }
         public bool Dead { get; private set; } = false;
         public AnimalHistory AnimalHistory { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        public void UpdateApproximateAge(string approximateAge)
+        {
+            this.ApproximateAge = ApproximateAge;
+        }
 
         public void Castrate(DateTime procedureDate, string observation)
         {
             this.Castrated = true;
-            UpdateAnimalHistoryItem(Guid.NewGuid(), "Castrado", observation);
+            AddAnimalHistoryItem("Castrado", observation);
         }
 
-        public bool UpdateAnimalHistoryItem(Guid id, string title, string report)
+        public void AnimalDied(DateTime date, string cause)
         {
-            return this.AnimalHistory.UpdateItem(id, title, report);
+            this.Dead = true;
+            AddAnimalHistoryItem("Morreu", cause);
+        }
+
+        public void AddAnimalHistoryItem(string title, string report)
+        {
+            AnimalHistory.AddItem(title, report);
+        }
+
+        private bool BirthdayIsToday()
+        {
+            return this.Birthday.Value.Date == DateTime.Today.Date;
         }
     }
 }
