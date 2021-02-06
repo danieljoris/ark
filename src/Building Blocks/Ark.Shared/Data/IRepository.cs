0bc5da28b817f77ce73e8afb7b81900a5617f1bd
Daniel Joris <danieljoris08@gmail.com>
2021-02-06 14:00:11 -0300

    Adicionado Interface genérica para Repositórios e padrão UnitOfWork.

---
+++ src/Building Blocks/Ark.Shared/Data/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ark.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : Entity
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
