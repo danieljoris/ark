5639ab63d6b677ae70c63ccd03fbb66fd395faa8
Daniel Joris <danieljoris08@gmail.com>
2021-02-06 14:06:04 -0300

    Adicionado padrão Mediator.

---
+++ src/Building Blocks/Ark.Shared/Mediator/IMediatorHandler.cs
@@ -0,0 +1,15 @@
+﻿using Ark.Core.Messages;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Core.Mediator
+{
+    public interface IMediatorHandler
+    {
+        Task<ValidationResult> SendCommand<T>(T command) where T : Command;
+    }
+}
---
+++ src/Building Blocks/Ark.Shared/Mediator/MediatorHandler.cs
@@ -0,0 +1,26 @@
+﻿using Ark.Core.Messages;
+using FluentValidation.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Core.Mediator
+{
+    public class MediatorHandler : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public MediatorHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<ValidationResult> SendCommand<T>(T command) where T : Command
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
