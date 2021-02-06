bc690f78610c4cfff74c3f9798de5cc3cbe008bb
Daniel Joris <danieljoris08@gmail.com>
2021-02-06 14:05:34 -0300

    Adicionado padrão CQS.

---
+++ src/Building Blocks/Ark.Shared/Messages/Command.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Core.Messages
+{
+    public abstract class Command : IRequest<ValidationResult>
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+
+        public virtual bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Building Blocks/Ark.Shared/Messages/CommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Ark.Core.Data;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ark.Core.Messages
+{
+    public abstract class CommandHandler
+    {
+        protected ValidationResult ValidationResult;
+
+        protected CommandHandler()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        protected void AddError(string message)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(string.Empty, message));
+        }
+
+        protected void AddError(string property, string message)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(property, message));
+        }
+
+        protected void AddError(string property, string message, object attemptedValue)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(property, message, attemptedValue));
+        }
+
+        public async Task<ValidationResult> PersistData(IUnitOfWork uow)
+        {
+            if (!await uow.Commit()) AddError("Houve um erro ao persistir os dados.");
+
+            return ValidationResult;
+        }
+    }
+}
