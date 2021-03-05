aea2cc6ca68dfbf38a40512b0ab6b3ff910e8282
Daniel Joris <danieljoris08@gmail.com>
2021-03-05 11:10:29 -0300

    Renamed/moved Ark.Shared to Ark.Core

--- src/Building Blocks/Ark.Core/Entities/Entity.cs
+++ src/Building Blocks/Ark.Core/Entities/Entity.cs
@@ -5,7 +5,6 @@ namespace Ark.Core.Entities
     public abstract class Entity
     {
         public Guid Id { get; private set;  }
-
         public DateTime CreatedAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
         public DateTime DeletedAt { get; private set; }
@@ -13,8 +12,8 @@ namespace Ark.Core.Entities
 
         public Entity()
         {
-            this.Id = Guid.NewGuid();
-            this.CreatedAt = DateTime.UtcNow;
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
         }
     }
 }
